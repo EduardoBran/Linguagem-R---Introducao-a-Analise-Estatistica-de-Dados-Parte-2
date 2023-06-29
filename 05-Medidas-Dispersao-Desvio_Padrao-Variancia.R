357e5a41316237521cadb579a6c7a35dbf9b5e99
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-06-29 17:39:22 -0300

    medidas de dispersao - desvio padrao e variancia - parte 1

---
+++ 05-Medidas-Dispersao-Desvio_Padrao-Variancia.R
@@ -0,0 +1,22 @@
+# Medidas de Dispersão - Desvio Padrão e Variância
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/9.Introducao-a-Analise-Estatistica-de-Dados---Parte-2")
+getwd()
+
+
+# - As medidas de tendência central (moda, valores max e min, amplitude, quartil e percentil) nos ajudam a descrever a 
+#   a centralidade e posicionamento dos dados.
+
+# - Logo, as medidas de tendência central seriam suficientes para descrever os dados? Não.
+# - Precisamos também das medidas de dispersão (desvio padrão e variância)
+
+# - Combinando as medidas de tendência central e dispersão, obtemos uma visão mais abrangente das características dos dados.
+
+
+
+##### Desvio Padrão #####
+
+# - O desvio padrão indica o grau de variação de um conjunto de dados, este conjunto pode ser amostral ou ou populacional.
+
+# 
