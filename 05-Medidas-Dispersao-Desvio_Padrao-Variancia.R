a080647be7054afde0a5ded4028aeda0e56a133f
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-06-30 17:41:27 -0300

    medidas de dispersao - desvio padrao e variancia - parte 2

--- 05-Medidas-Dispersao-Desvio_Padrao-Variancia.R
+++ 05-Medidas-Dispersao-Desvio_Padrao-Variancia.R
@@ -14,9 +14,50 @@ getwd()
 # - Combinando as medidas de tendência central e dispersão, obtemos uma visão mais abrangente das características dos dados.
 
 
-
 ##### Desvio Padrão #####
 
 # - O desvio padrão indica o grau de variação de um conjunto de dados, este conjunto pode ser amostral ou ou populacional.
 
-# 
+# - Para um conjunto amostral o desvio padrão é dado pelo somatório da razi quadrada do quadrado da diferença entre o valor do
+#   dado coletado (xi) e o valor médio (x), dividido pelo tamanho amostral menos um (n - 1).
+
+# - O desvio padrão mede a dispersão dos dados em relação à média. Quanto maior o desvio padrão, maior é a variação dos dados
+#   em relação à média, indicando maior dispersão ou heterogeneidade dos valores. Por outro lado, um desvio padrão menor indica
+#   menor variação dos dados em relação à média, indicando maior homogeneidade ou consistência dos valores.
+
+# - O desvio padrão é uma medida importante para entender a dispersão dos dados e avaliar a variabilidade dos valores em um
+#   conjunto de dados.
+
+
+# Exemplo: Um engenheiro precisa decidir entre três modelos de máquinas de corte de alta precisão, para isso ele usa como
+#          critério o desvio padrão. A máquina que tiver menor desvio será a escolhida por ele. A partir dos dados de
+#          medida de corte das 3 máquinas em milímetros, determine qual deve ser a escolhida pelo engenheiro.
+
+# Máquina 1 (mm) = (181.9, 180.8, 181.9, 180.2, 181.4). 
+# Máquina 2 (mm) = (180.1, 181.8, 181.5, 181.2, 182.4). 
+# Máquina 3 (mm) = (182.1, 183.7, 182.1, 180.2, 181.9).
+
+Maq1 = c(181.9, 180.8, 181.9, 180.2, 181.4)
+Maq2 = c(180.1, 181.8, 181.5, 181.2, 182.4)
+Maq3 = c(182.1, 183.7, 182.1, 180.2, 181.9)
+
+mean(Maq1)  # 181.24
+mean(Maq2)  # 181.4
+mean(Maq3)  # 182
+
+# - Neste caso apenas com os valores das médidas de cada corte de máquina, não conseguiríamos responder a pergunta de negócio,
+#   nem se usássemos a mediana ou moda porque não temos como saber a variebilidade (poderíamos ter valores extremos que
+#   estão afetando a média)
+
+sd(Maq1)    # 0.7368853
+sd(Maq2)    # 0.8514693
+sd(Maq3)    # 1.240967
+
+# - Agora com os resultados do desvio padrão de cada máquina, detectamos que a Maq1 tem o menor valor de variabilidade e
+#   por isso tem um nível maior de precisão.
+
+
+
+
+
+
