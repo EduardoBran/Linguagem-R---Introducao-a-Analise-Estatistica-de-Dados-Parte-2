64cab6ac99e64e765b9dea7a663e59e6577f73c7
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-07-12 17:02:33 -0300

    conceito probabilidade x possibilidade

---
+++ 13-Probabilidade_Conceito.R
@@ -0,0 +1,18 @@
+#  Conceito Probabilidade
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/9.Introducao-a-Analise-Estatistica-de-Dados---Parte-2")
+getwd()
+
+
+
+# - Primeiramente vamos perguntar: Probabilidade e Possibilidade são a mesma coisa ?
+# - Não!
+
+
+
+# - Probabilidade é a medida da possibilidade de um evento ocorrer.
+# - Em outras palavras, se a chance de chover amanhã é de 40%, há menos possibilidades que chova amanhã, do que não chova.
+
+
+
