0902eb15935e088c1a31d979fa9ea23c1374728e
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-07-21 15:28:58 -0300

    conceito tipos de distribuicao de probabilidade

---
+++ 15-Distribuicao_de_Probabilidade.R
@@ -0,0 +1,45 @@
+# Tipos de Distribuição de Probabilidade
+
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/9.Introducao-a-Analise-Estatistica-de-Dados---Parte-2")
+getwd()
+
+
+
+# - Em estatística, uma Distribuição de Probabilidade descreve a chance que uma variável pode assumir ao longo de um espaço de valores.
+
+# - A Distribuição de Probabilidades tem por objetivo associar uma probabilidade a cada resultado numérico de um experimento.  
+
+# - Ou seja, é mais ou menos como se realizassemos o mesmo experimento muitas e muitas vezes e assim obter todas as possíveis probabilidades
+#   e assim criar uma Distribuição de modo que na hora de se perguntar "qual a probabilidade da variável de valor X?", assim nós vamos até a
+#   distribuição e buscamos esse valor.
+
+
+# - Ela é uma função cujo domínio são os valores da variável e cuja imagem são as probabilidades de a variável assumir cada valor do domínio.
+#   O conjunto imagem deste tipo de função está sempre restrito ao intervalo entre 0 e 1.
+
+
+# - A soma de todos os valores de uma Distribuição de Probabilidades deve ser igual a 1.
+# - A Probabilidade Ocorrência de um evento deve ser maior que 0 e menor do que 1.
+
+
+# - Uma distribuição de probabilidades pode ser Discreta, Contínua ou Normal.
+
+#  -> Discreta: descreve quantidades aleatórias de dados que podem assumir valores finitos.
+#     Principais tipos: Binomial, Poisson, Hipergeométrica, Bernoulli
+
+#  -> Contínua: descreve quantidades aleatórias de dados que podem assumir valores infinitos.
+#     Principais tipos: Uniforme, Exponencial, Gama, Chi-Quadrado
+
+#  -> Normal: uma variável randômica contínua que segue uma Distribuição de Probabilidade Normal tem uma série de características distintas.
+
+# - Varios algoritmos de aprendizado de máquinas esperam que os dados estejam em um formato de Distribuição Normal, se não tiverem é trabalho
+#   do Cientista de Dados aplicar normalização (deixar os dados em um formato normal).
+
+
+
+
+
+
+
