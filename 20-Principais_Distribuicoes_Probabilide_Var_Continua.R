c45d1787c11683e9af45bdcb93d25e19ae260045
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-08-08 17:10:27 -0300

    conceito principais distribuicoes probabilide var continuas

---
+++ 20-Principais_Distribuicoes_Probabilide_Var_Continua.R
@@ -0,0 +1,27 @@
+# # Exemplos das Principais Distribuições de Probabilidade para Variáveis Aleatórias Contínuas
+
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/9.Introducao-a-Analise-Estatistica-de-Dados---Parte-2")
+getwd()
+
+
+
+# - Até aqui vimos as principais distribuições de probabilididade para variaveis aleatórias discretas.
+
+
+# - Agora vamos ver as principais distribuições de probabilididade para variaveis aleatórias contínuas.
+
+
+# - Os 3 formatos mais comuns são:
+
+#  -> Distribuição Normal
+#  -> Distribuição Exponencial
+#  -> Distribuição Uniforme
+
+
+## Distribuição Normal
+
+# - É a mais importante de todas, seja entre as discretas ou as contínuas.
+#   É útil quando os dados tendem a estar próximos ao centro da distribuição (próximos da média) e quando valores extremos (outliers) são
+#   muito raros.
